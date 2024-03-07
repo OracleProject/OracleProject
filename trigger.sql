c5387d2378e0ea7d9b120f6e01a60ac21f1a6980
juniel1299 <youngs5440@gmail.com>
2024-03-07 18:36:14 +0900

    ??

--- trigger.sql
+++ trigger.sql
@@ -1,8 +1,4 @@
---Trigger
-
---3번
-
-/
+--1번
 CREATE OR REPLACE TRIGGER trgCheckPeriodSum
 BEFORE INSERT OR UPDATE ON tblcourseperiod
 FOR EACH ROW
@@ -19,13 +15,17 @@ BEGIN
     END IF;
 END;
 /
+select * from tblopencurriculum;
 
-/
+
+select
+sum(enddate - startdate)
+from tblopencurriculum;
+
+1223/30;
 
 
---6번
-
-/
+--2번
 -- 점수 배점 용
 CREATE OR REPLACE TRIGGER trgCheckPoints
 BEFORE INSERT ON tblTestInfo
@@ -36,24 +36,26 @@ BEGIN
     -- writtenpoints, practicalpoints, attendancepoints의 합을 계산합니다.
     v_total_points := :NEW.writtenpoints + :NEW.practicalpoints + :NEW.attendancepoints;
 
-    IF :NEW.writtenpoints >=  THEN
-        RAISE_APPLICATION_ERROR(-20001, '최대 30까지 입력 가능합니다.');
-    END IF;
-    
-    IF :NEW.practicalpoints >= 40 THEN
+    IF :NEW.writtenpoints >= 40 THEN
         RAISE_APPLICATION_ERROR(-20002, '최대 40까지 입력 가능합니다.');
     END IF;
-    
+
+    IF :NEW.practicalpoints >= 40 THEN
+        RAISE_APPLICATION_ERROR(-20003, '최대 40까지 입력 가능합니다.');
+    END IF;
+
     IF :NEW.attendancepoints >= 30 THEN
-        RAISE_APPLICATION_ERROR(-20003, '최대 30까지 입력 가능합니다.');
+        RAISE_APPLICATION_ERROR(-20004, '최대 30까지 입력 가능합니다.');
     END IF;
 
     -- writtenpoints, practicalpoints, attendancepoints의 합이 100을 초과하는 경우 오류를 발생시킵니다.
     IF v_total_points > 100 THEN
-        RAISE_APPLICATION_ERROR(-20004, '총점이 100을 초과할 수 없습니다.');
+        RAISE_APPLICATION_ERROR(-20005, '총점이 100을 초과할 수 없습니다.');
     END IF;
 END;
 /
+
+-- 3번 (수정필요!)
 -- 학생 성적 insert 용 
 CREATE OR REPLACE TRIGGER trgTraineeCheckPoints
 BEFORE INSERT ON tblgrades
@@ -65,26 +67,27 @@ BEGIN
     v_total_grade := :NEW.writtengrade + :NEW.practicalgrade + :NEW.attendancegrade;
 
     IF :NEW.writtengrade > 40 THEN
-        RAISE_APPLICATION_ERROR(-20001, '최대 40까지 입력 가능합니다.');
+        RAISE_APPLICATION_ERROR(-20006, '최대 40까지 입력 가능합니다.');
     END IF;
     
     IF :NEW.practicalgrade > 40 THEN
-        RAISE_APPLICATION_ERROR(-20002, '최대 40까지 입력 가능합니다.');
+        RAISE_APPLICATION_ERROR(-20007, '최대 40까지 입력 가능합니다.');
     END IF;
     
     IF :NEW.attendancegrade > 30 THEN
-        RAISE_APPLICATION_ERROR(-20003, '최대 30까지 입력 가능합니다.');
+        RAISE_APPLICATION_ERROR(-20008, '최대 30까지 입력 가능합니다.');
     END IF;
 
     -- writtengrade, practicalgrade, attendancegrade의 합이 100을 초과하는 경우 오류를 발생시킵니다.
     IF v_total_grade > 100 THEN
-        RAISE_APPLICATION_ERROR(-20004, '총점이 100을 초과할 수 없습니다.');
+        RAISE_APPLICATION_ERROR(-20009, '총점이 100을 초과할 수 없습니다.');
     END IF;
 END;
 /
-/
--- 7번 
-/
+
+
+-- 4번 
+-- 수료자만 교육 과정 과정 평가를 할 수 있으므로 수료 완료인지 확인하고 수료 완료가 아닐 경우 insert하지 못하도록 하는 트리거
 CREATE OR REPLACE TRIGGER trgAllowEvaluation
 BEFORE INSERT ON tblcurriculumevaluation
 FOR EACH ROW
@@ -98,16 +101,14 @@ BEGIN
 
     -- status가 '수료'인 경우에만 데이터를 삽입할 수 있습니다.
     IF v_status <> '수료' THEN
-        RAISE_APPLICATION_ERROR(-20001, '수료 상태일 때만 평가를 입력할 수 있습니다.');
+        RAISE_APPLICATION_ERROR(-20010, '수료 상태일 때만 평가를 입력할 수 있습니다.');
     END IF;
 END;
 /
 
 
-
-
--- 8번
-/
+-- 5번
+-- 성적은 종료 후 입력이 되니까 그 전에 입력 하지 못하도록 하는 트리거 
 CREATE OR REPLACE TRIGGER trgCheckEnddate
 BEFORE INSERT ON tblTestInfo
 FOR EACH ROW
@@ -121,15 +122,16 @@ BEGIN
     
     -- 종료일(enddate) 이후에만 삽입을 허용합니다.
     IF v_enddate IS NOT NULL AND v_enddate <= SYSDATE THEN
-        RAISE_APPLICATION_ERROR(-20001, '과목 종료일 이후에만 시험 정보를 삽입할 수 있습니다.');
+        RAISE_APPLICATION_ERROR(-20011, '과목 종료일 이후에만 시험 정보를 삽입할 수 있습니다.');
     END IF;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-        RAISE_APPLICATION_ERROR(-20002, '해당 과목 ID가 tblOpensubjectList에 존재하지 않습니다.');
+        RAISE_APPLICATION_ERROR(-20012, '해당 과목 ID가 tblOpensubjectList에 존재하지 않습니다.');
 END;
 /
 
---9번
+
+-- 6번
 -- 중도탈락 된 학생은 중도탈락 날짜 이후 더 이상 성적 입력을 할 수 없음에 대한 트리거
 CREATE OR REPLACE TRIGGER trgCheckMidtermExit
 BEFORE INSERT OR UPDATE ON tblTestInfo
@@ -146,79 +148,12 @@ BEGIN
     -- status가 '중도탈락'인 경우에만 성적을 삽입할 수 있습니다.
     IF v_status = '중도탈락' THEN
         -- writtendate와 practicaldate 값을 가져와서 비교합니다.
-        IF (:NEW.writtendate = v_day OR :NEW.practicaldate = v_day) THEN
-            RAISE_APPLICATION_ERROR(-20001, '중도탈락 상태일 때는 해당 날짜의 성적을 삽입할 수 없습니다.');
+        IF (:NEW.writtendate > v_day OR :NEW.practicaldate > v_day) THEN
+            RAISE_APPLICATION_ERROR(-20013, '중도탈락 상태일 때는 해당 날짜의 성적을 삽입할 수 없습니다.');
         END IF;
     END IF;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-        RAISE_APPLICATION_ERROR(-20002, '해당 trainee_id가 tbltraineelist에 존재하지 않습니다.');
+        RAISE_APPLICATION_ERROR(-20014, '해당 trainee_id가 tbltraineelist에 존재하지 않습니다.');
 END;
-/
-
-
-/
-
--- 10번
-/
-
-/
-CREATE OR REPLACE PROCEDURE procUpdateaAttendanceStatuss
-IS
-    vInTime tblAttendance.inTime%TYPE;
-    vOutTime tblAttendance.outTime%TYPE;
-    vSeq_attendanceStatus tblAttendance.seq_attendanceStatus%TYPE;
-    vCurrentTime VARCHAR2(2);
-
-    CURSOR vcursor IS
-        SELECT 
-            inTime,
-            outTime,
-            seq_attendanceStatus
-        FROM tblAttendance
-        WHERE inTime IS NULL OR outTime IS NULL
-        FOR UPDATE;
-
-BEGIN
-    SELECT TO_CHAR(SYSDATE, 'hh24') INTO vCurrentTime FROM dual;
-
-    FOR rec IN vcursor LOOP
-        vInTime := rec.inTime;
-        vOutTime := rec.outTime;
-
-        -- inTime이 null이고 현재 시간이 9시 이후인 경우
-        IF vInTime IS NULL AND TO_NUMBER(vCurrentTime) >= 9 THEN
-            vSeq_attendanceStatus := 2; -- 지각
-        END IF;
-
-        -- outTime이 null이고 현재 시간이 18시 이후인 경우
-        IF vOutTime IS NULL AND TO_NUMBER(vCurrentTime) >= 18 THEN
-            vSeq_attendanceStatus := 3; -- 조퇴
-        END IF;
-
-        -- intime과 outtime이 모두 null이고 현재 시간이 18시 이후인 경우
-        IF vInTime IS NULL AND vOutTime IS NULL AND TO_NUMBER(vCurrentTime) >= 18 THEN
-            vSeq_attendanceStatus := 6; -- 결석
-        END IF;
-
-        -- 출결 상태가 업데이트되었다면
-        IF vSeq_attendanceStatus IS NOT NULL THEN
-            UPDATE tblAttendance
-            SET seq_attendanceStatus = vSeq_attendanceStatus
-            WHERE CURRENT OF vcursor;
-        END IF;
-    END LOOP;
-
-    -- 모든 업데이트가 완료된 후 커밋
-    COMMIT;
-END;
-
-
-/
-
-insert into tblAttendance
-values(
-111113,51,null,to_date('2024-03-07','YYYY-MM-DD'),to_date('2024-03-07 08:47:44','YYYY-MM-DD HH24:MI:SS'),null);
-/
-
 /
